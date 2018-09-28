a492aee436eb363dc76c3726a70d06591fa38167
zanema <amenaz1@gmail.com>
2018-09-28 14:21:01 -0500

    Revert "Merge pull request #1 from teamnsrg/kumarde/update_downloader"
    This reverts commit bd0cf8dccb333d7781ef392d5decfe461332e24a, reversing
    changes made to 9d6b3d35830afd47a17a6f1b6a91c7a57658ec84.

--- cmd/sbserver/main.go
+++ cmd/sbserver/main.go
@@ -197,7 +197,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"path"
 
 	"github.com/teamnsrg/safebrowsing"
 	pb "github.com/teamnsrg/safebrowsing/internal/safebrowsing_proto"
@@ -221,10 +220,10 @@ const (
 )
 
 var (
+	apiKeyFlag   = flag.String("apikey", "", "specify your Safe Browsing API key")
 	srvAddrFlag  = flag.String("srvaddr", "localhost:8080", "TCP network address the HTTP server should use")
 	proxyFlag    = flag.String("proxy", "", "proxy to use to connect to the HTTP server")
 	databaseFlag = flag.String("db", "", "path to the Safe Browsing database.")
-	dbDir        = flag.String("dbdir", "/data1/nsrg/safebrowsing/archive", "path to the Safe Browsing DB Archive")
 )
 
 var threatTemplate = map[safebrowsing.ThreatType]string{
@@ -495,38 +494,21 @@ func serveRedirector(resp http.ResponseWriter, req *http.Request, sb *safebrowsi
 	http.Error(resp, err.Error(), http.StatusInternalServerError)
 }
 
-func getLatestDBVersion(archivePath string) string {
-	files, err := ioutil.ReadDir(archivePath)
-	if err != nil {
-		fmt.Printf("error in reading archivePath %s\n", archivePath)
-	}
-	if len(files) <= 0 {
-		fmt.Fprintln(os.Stderr, "db directory is empty - aborting")
-		os.Exit(1)
-	}
-
-	return path.Join(archivePath, files[len(files)-1].Name())
-
-}
-
 func main() {
-
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, usage, os.Args[0])
 		flag.PrintDefaults()
 	}
 	flag.Parse()
-
-	if len(*dbDir) <= 0 {
-		fmt.Fprintln(os.Stderr, "dbDir (-dbdir flag) required")
+	if *apiKeyFlag == "" {
+		fmt.Fprintln(os.Stderr, "No -apikey specified")
 		os.Exit(1)
 	}
-
 	conf := safebrowsing.Config{
+		APIKey:   *apiKeyFlag,
 		ProxyURL: *proxyFlag,
-		DBPath:   getLatestDBVersion(*dbDir),
+		DBPath:   *databaseFlag,
 		Logger:   os.Stderr,
-		DBDir:    *dbDir,
 	}
 	sb, err := safebrowsing.NewSafeBrowser(conf)
 	if err != nil {
