6159410e6997b3c65347a69983e12cfb920f1a00
gayde2 <gayde2@illinois.edu>
2018-10-05 17:03:01 -0500

    working on python

--- cmd/sbserver/parse-csv.py
+++ cmd/sbserver/parse-csv.py
@@ -2,12 +2,26 @@ import requests
 import json
 import csv
 
-url = "127.0.0.1:8080/v4/threatMatches:find"
+url = "https://safebrowsing.googleapis.com/v4/fullHashes:find?key=AIzaSyC4EjprVfp6YX6aSlHqFZhaUrmbgRMoi7w"
 
 x = {
+
+        'client':{
+        'clientId': 'IllinoisNSRG',
+        'clientVersion': '1.0',
+        },
 	'threatInfo': {
-	'threatTypes': ['ANY_TYPE'], #TODO check this
-		'threadEntries': []
+	'threatTypes': ['THREAT_TYPE_UNSPECIFIED'],
+	'platformTypes': ['ANY_PLATFORM'],
+	'threatEntryTypes':['URL'],
+	'threatEntries': [
+      {"hash": "WwuJdQ=="},
+      {"hash": "771MOg=="},
+      {"hash": "5eOrwQ=="}	]
+	},
+	'apiClient':{
+	'clientId': 'IllinoisNSRG',
+	'clientVersion': '1.0',
 	}
 }
 
@@ -25,16 +39,19 @@ with open('blacklist-entries.csv') as csvfile:
 			break
 		if(row[0][:2] == '//'):
 			newEntry = 	{'url':row[0][2:]}
-			
+
 		else:
 			newEntry = 	{'url':row[0]}
 
-		#print(newEntry)	
+		#print(newEntry)
 		entries.append(newEntry)
 		i = i + 1;
 #print(entries)
 
-x['threatInfo']['threadEntries'] = entries
-request = requests.post(url, params=x)
-print request.txt
-#print (json.dumps(x))
+
+#x['threatInfo']['threatEntries'] = entries
+#print(x)
+response = requests.post(url, json=x)
+print (response.text)
+#print (response.json())
+print (json.dumps(x))
