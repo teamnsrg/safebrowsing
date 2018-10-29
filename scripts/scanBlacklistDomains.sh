a38bca540a93f60d28d853325348a47938e94136
zanema <amenaz1@gmail.com>
2018-10-29 15:47:01 -0500

    fix script, remove apikey

--- scripts/scanBlacklistDomains.sh
+++ scripts/scanBlacklistDomains.sh
@@ -6,7 +6,6 @@ export GOPATH="/home/zzma/src/golang"
 DATE=`date -u +%Y-%m-%d`
 DATETIME=`date -u +%Y-%m-%dT%H:%M:%S`
 EXEC_PATH="$GOPATH/src/github.com/teamnsrg/safebrowsing/cmd/sblookup"
-API_KEY_FILE="$GOPATH/src/github.com/teamnsrg/safebrowsing/.api/sb-api.key"
 DOWNLOAD_PATH="/data1/nsrg/domain_blacklists/safebrowsing"
 HOSTNAMES_FILE="hostnames.txt"
 RESULTS_FILE="results.txt"
@@ -23,7 +22,7 @@ fi
 
 cd $EXEC_PATH
 go build
-$EXEC_PATH/sblookup -apikey $(cat $API_KEY_FILE) -server http://localhost:8080 -input $DOWNLOAD_PATH/$DATETIME-$HOSTNAMES_FILE -output $DOWNLOAD_PATH/$DATETIME-$RESULTS_FILE > $LOG_PATH/$DATETIME.log 2>&1
+$EXEC_PATH/sblookup -server http://localhost:8080 -input $DOWNLOAD_PATH/$DATETIME-$HOSTNAMES_FILE -output $DOWNLOAD_PATH/$DATETIME-$RESULTS_FILE > $LOG_PATH/$DATETIME.log 2>&1
 
 if [ "$1" != 0 ]; then
     cat $LOG_PATH/$DATETIME.log | mail -s "Safebrowsing Script Failed" zanema2@illinois.edu
