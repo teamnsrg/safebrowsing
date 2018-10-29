238e98d4cb67e08f1c4a661fddf348a5bd463eb4
zanema <amenaz1@gmail.com>
2018-10-29 16:45:41 -0500

    download urls for safebrowing lookups

--- scripts/scanBlacklistDomains.sh
+++ scripts/scanBlacklistDomains.sh
@@ -7,13 +7,13 @@ DATE=`date -u +%Y-%m-%d`
 DATETIME=`date -u +%Y-%m-%dT%H:%M:%S`
 EXEC_PATH="$GOPATH/src/github.com/teamnsrg/safebrowsing/cmd/sblookup"
 DOWNLOAD_PATH="/data1/nsrg/domain_blacklists/safebrowsing"
-HOSTNAMES_FILE="hostnames.txt"
+URLS_FILE="urls.txt"
 RESULTS_FILE="results.txt"
 LOG_PATH="/data1/nsrg/domain_blacklists/safebrowsing/logs"
 
 
 # Export hostnames from redis
-redis-cli --raw smembers hostnames > $DOWNLOAD_PATH/$DATETIME-$HOSTNAMES_FILE
+redis-cli --raw smembers urls > $DOWNLOAD_PATH/$DATETIME-$URLS_FILE
 
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
@@ -22,4 +22,4 @@ fi
 
 cd $EXEC_PATH
 go build
-$EXEC_PATH/sblookup -server http://localhost:8080 -input $DOWNLOAD_PATH/$DATETIME-$HOSTNAMES_FILE -output $DOWNLOAD_PATH/$DATETIME-$RESULTS_FILE > $LOG_PATH/$DATETIME.log 2>&1
+$EXEC_PATH/sblookup -server http://localhost:8080 -input $DOWNLOAD_PATH/$DATETIME-$URLS_FILE -output $DOWNLOAD_PATH/$DATETIME-$RESULTS_FILE > $LOG_PATH/$DATETIME.log 2>&1
